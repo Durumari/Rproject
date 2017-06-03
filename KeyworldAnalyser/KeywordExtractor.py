21e9d781d52b13bca26e02f13f03aa9e7592b575
IkbomJeon <jeonikbeom@gmail.com>
2017-06-03 21:54:40 +0900

    #ImportData 함수 파라메터 수정 등.

--- KeyworldAnalyser/KeywordExtractor.py
+++ KeyworldAnalyser/KeywordExtractor.py
@@ -7,20 +7,37 @@ from konlpy.tag import Kkma
 from konlpy.utils import pprint
 import operator
 import csv
-import gc
-import jpype
 
 
-def ImportData(directory, prefixName,searchingKeyword, firstday, lastday):
+def ImportData(directory, prefixName,searchingKeyword, year, firstday, lastday):
     data_for_days = []
  
-      #날짜 범위 입력 
-    for i in range (firstday, lastday):
-        day = "{:04d}".format(i)
-        filepath = directory + prefixName + '_' + day + searchingKeyword +'.csv'
-                
-        data = pd.read_csv(filepath, encoding='CP949') # encoding for korean.
+    from_date = datetime.datetime.strptime(str(year)+str(firstday), "%Y%m%d").date()
+    to_date = datetime.datetime.strptime(str(year)+str(lastday), "%Y%m%d").date()
     
+    delta = to_date - from_date
+    
+    list_between_days = []
+    for i in range(0, delta.days + 1):
+        list_between_days.append(from_date + datetime.timedelta(days=i))
+    
+    print(list_between_days)
+    
+      #날짜 범위 입력 
+    #for i in range (firstday, lastday):
+
+    for d0 in list_between_days:
+        
+        strDate = "{:02d}".format(d0.month) + "{:02d}".format(d0.day)
+        #day = "{:04d}".format(i)
+        print(strDate)
+        filepath = directory + prefixName  + searchingKeyword + '_' + strDate +'.csv'
+                
+        #ufilepath = filepath.encode('CP949')
+   
+        print(filepath)                
+        data = pd.read_csv(filepath, encoding='CP949') # encoding for korean.
+        
         data_for_days.append(data)
     
     return pd.concat(data_for_days, ignore_index = True) 
@@ -30,14 +47,21 @@ if __name__ == "__main__":
     #########################################################
     # raw data import
     #########################################################
-    _directory = '../TwitterCrawling/'
-    _prefixName = 'twitterlist'
-    _searchingKeyword = ['', '_dust']
-    _firstday= 518 # 17일 data '초'가 없어서 일단 제외함.
-    _lastday=527
-   
-    raw_data = ImportData(_directory, _prefixName, _searchingKeyword[0], _firstday, _lastday)
-         
+    _directory = '../TwitterCrawling/twitterData_0601/'
+    _prefixName = u'twitterlist'
+    _list_searchingKeyword = [u'_kangnamyeock', '_angoo', '_baljeonso', '_gongichungjunggi',
+                              '_hwangsa', '_kyeonyucha', '_mask','_muckum', '_muckutda', '_joonggook', '_miyeockgook'
+                              ,'_samgyeopsal']
+    
+    _searchingKeyword = _list_searchingKeyword[9]
+    
+    _year = 2017
+    _firstday = 525 
+    _lastday = 531 # 폐구간 [firstday, lastday]
+
+    
+    raw_data = ImportData(_directory, _prefixName, _searchingKeyword, _year, _firstday, _lastday) 
+    #print(raw_data)
 
     #print(raw_data['created'])
     
@@ -45,61 +69,81 @@ if __name__ == "__main__":
     #형태소 분석해서 날짜별로 키워드 빈도 추출
     #########################################################
     
-  
+    print('$All data is imported.')
     # 일단 string type을 datetime type으로 변환.
     convertied_date =[]
+    prevtime = datetime.datetime(2010, 9, 12, 11, 19, 54)
+    #print(prevtime)
     for idx in range(0, len(raw_data)):
-        convertied_date.append(datetime.datetime.strptime(raw_data['created'][idx], "%Y-%m-%d %H:%M:%S").date())
+        print(str(idx) + "th ")
+        if len(str(raw_data['created'][idx])) <= len('0000-00-00 00:00'):
+            print(raw_data['created'][idx])
+            convertied_date.append(datetime.datetime.strptime(raw_data['created'][idx], "%Y-%m-%d %H:%M").date())
+            #prevtime = datetime.datetime.strptime(raw_data['created'][idx], "%Y-%m-%d %H:%M").date()
+           # print(datetime.datetime.strptime(raw_data['created'][idx], "%Y-%m-%d %H:%M").date())
+        elif len(str(raw_data['created'][idx])) <= len('0000-00-00 00:00:00'):
+            print(raw_data['created'][idx])
+            convertied_date.append(datetime.datetime.strptime(raw_data['created'][idx], "%Y-%m-%d %H:%M:%S").date())
+            #prevtime = datetime.datetime.strptime(raw_data['created'][idx], "%Y-%m-%d %H:%M").date()
+            #print(datetime.datetime.strptime(raw_data['created'][idx], "%Y-%m-%d %H:%M").date())
+        #else:
+            #convertied_date.append(prevtime) #현재 레코드의 시간이 엉뚱하게 저장되어있을 경우 그냥 이전값으로. 
+            
         
-    raw_data['date'] = convertied_date # datatime 타입의 새로운 column 생성. 
-        
-    lebels, uniques = pd.factorize(raw_data['date'])
+            
     
+    #print(0)
+    #print (convertied_date)
+    
+    #print(len(raw_data['created']))
+    #print(len(convertied_date))
+    raw_data['date'] = convertied_date # datatime 타입의 새로운 column 생성. 
+    #print(1)
+    lebels, uniques = pd.factorize(raw_data['date'])
+    #print(uniques)
 
     # 일별로 text를 merge해서 명사 추출
-    kkma = Kkma()
     
-    for i in range(4, len(uniques)):
-        
+    kkma = Kkma()
+    for i in range(0, len(uniques)):
+        print(str(uniques[i]))
         dictionary_keyword ={}
         df = raw_data.loc[raw_data['date'] ==  uniques[i]]
         
-        
-        bunddle_text = ""
-        for text in df['text']:
-            
-            #if temp_cnt > 3:
-                #break;
-            #nouns = kkma.nouns(text)
-            bunddle_text +=(text + " ")
-            
-        nouns = kkma.nouns(bunddle_text) 
-        #print(bunddle_text)
-        #fn = 'test_' + str(uniques[i]) + '.txt'
-        #with open(fn, 'w') as fp:
-            #fp.write(bunddle_text)
-        
         temp_cnt = 0
-        for n in nouns:
-            if n in dictionary_keyword:
-                dictionary_keyword[n] += 1
-            else:
-                dictionary_keyword[n] = 0
-            
+        for text in df['text']:
+            temp_cnt += 1            
+            #if temp_cnt < 2260:
+                #continue;
+           
             print('#' + str(uniques[i])+' '+ str(temp_cnt) + ' th')
-            temp_cnt += 1
+            #print(text)
+            nouns = kkma.nouns(text)
+            
+            for n in nouns:
+                if n in dictionary_keyword:
+                    dictionary_keyword[n] += 1
+                else:
+                    dictionary_keyword[n] = 0
             
             
-        sorted_x = sorted(dictionary_keyword.items(), key=operator.itemgetter(1),  reverse=True)   
+            
+            
        
-        #csv로 저장
-        writefilename = 'frequency_keyword_' + str(uniques[i]) + '.csv'
+        sorted_x = sorted(dictionary_keyword.items(), key=operator.itemgetter(1),  reverse=True)   
         
+        writefilename = 'frequency_keyword_' +_prefixName +_searchingKeyword + '_' +str(uniques[i]) + '.csv'
+        
+         # csv로 저장
         with open(writefilename, 'w', newline='') as fp:
             csv_out = csv.writer(fp)
             csv_out.writerow(['keyword', 'frequency'])
-      
-            for i in range(0 , 500) : # 상위 500개
+     
+            if len(sorted_x[i]) > 500:
+                max = 500
+            else:
+                max = len(sorted_x[i])
+            for i in range(0 , max) : # 상위 500개
                 csv_out.writerow(sorted_x[i])
                 print(sorted_x[i])
 
